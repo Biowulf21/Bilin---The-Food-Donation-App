4a0b9cb97c6774c3b9f3449f07bd8c555e192399
James Jilhaney <20190016830@my.xu.edu.ph>
2022-05-16 17:08:44 +0800

    Added Signup user

--- public/src/landing_page/auth.js
+++ public/src/landing_page/auth.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
+// import { } from "https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"; 
+
 const firebaseconfig = {
     apiKey: "AIzaSyAtgFAs4hvlpMQStzVnD3tRJ9N8jLB98b0",
     authDomain: "bilin---the-food-donatio-a24c6.firebaseapp.com",
@@ -20,6 +22,7 @@ BtnLogin.addEventListener('click', loginWithEmailPassword);
 
 
 
+// Login
 async function loginWithEmailPassword() {
     const email = emaillogin.value;
     const password = passwordlogin.value;
@@ -27,24 +30,35 @@ async function loginWithEmailPassword() {
     try {
         const heheapp = initializeApp(firebaseconfig);
         const auth = getAuth(heheapp);
-        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+        // const userCredentials = await signInWithEmailAndPassword(auth, email, password);
         console.log('here3');
-        console.log(userCredentials.user);
+        // console.log(userCredentials.user);
     } catch (err) {
         console.log(err);
     }
 
 }
 
-async function createUserWithEmaillPass() {
+
+const donorBtn = document.getElementById('donor-sign-up-btn');
+donorBtn.addEventListener('click', createDonorWithEmaillPass);
+const donorName = document.getElementById('donor-full-name');
+const donorEmail = document.getElementById('donor-email');
+const donorNumber = document.getElementById('donor-number');
+const donorPassword = document.getElementById('donor-password');
+const donorConfirmPassword = document.getElementById('donor-confirm-password');
+// Sign Up as Donor
+async function createDonorWithEmaillPass() {
     try {
-        const email = 'testemail2@email.com';
-        const password = 'testpass2';
-        const heheapp = initializeApp(firebaseconfig);
-        const auth = getAuth(heheapp);
-        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-        console.log(userCredentials.user);
-        console.log('here create');
+        console.log('creatingUser')
+            const email = donorEmail.value;
+            const password = donorPassword.value
+            const bilinApp = initializeApp(firebaseconfig);
+            const auth = getAuth(bilinApp);
+            if (password !== donorConfirmPassword.value) return;
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+            console.log(userCredentials.user);
+            console.log('here create');
     } catch (err) {
         console.log(({
             'message': err.message,
@@ -53,11 +67,14 @@ async function createUserWithEmaillPass() {
     }
 }
 
+
+
 // const loginBTN = document.getElementById()
 var loginBtn = document.getElementById('login-btn');
 
+
 // loginWithEmailPassword();
-// createUserWithEmaillPass();
+createUserWithEmaillPass();
 
 
 
