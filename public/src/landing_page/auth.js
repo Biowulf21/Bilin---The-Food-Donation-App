603c1b0468dc58d2b94faaa6d78087b3d42e0ecf
zanne <tan.zanne99@gmail.com>
2022-06-05 21:00:40 +0800

    Working reusable auth modals

--- public/src/landing_page/auth.js
+++ public/src/landing_page/auth.js
@@ -34,151 +34,137 @@ const usersRef = collection(db, "Users");
 connectFirestoreEmulator(db, "localhost", 8080);
 connectAuthEmulator(auth, "http://localhost:9099");
 
-const emaillogin = document.getElementById("login_page_email_input");
-const passwordlogin = document.getElementById("login_page_password_input");
 
-//LOGIN
-$("#modal-login").on("shown.bs.modal", function () {
-  $("#login-btn-large").on("click", loginWithEmailPassword);
-});
-
-// const loginBTN = document.getElementById('login-btn')
-// loginBTN.addEventListener('click', loginWithEmailPassword)
-
-//PARTNER SIGNUP
-$("#modal-login").on("shown.bs.modal", function () {
-  const orgSignUpBtn = document.getElementById("partner-signup-btn");
-  orgSignUpBtn.addEventListener("click", createOrgWithEmaillPass);
-  // $('#')
-});
-
-$("#modal-login").on("shown.bs.modal", function () {
-  // DONOR SIGN UP
-  const donorBtn = document.getElementById("donor-sign-up-btn");
-  donorBtn.addEventListener("click", createDonorWithEmaillPass);
-});
-
-// Login
-async function loginWithEmailPassword() {
-  try {
-    const email = emaillogin.value;
-    const password = passwordlogin.value;
-    onAuthStateChanged(auth, async (user) => {
-      try {
-        if (!user) {
-          const userCredentials = await signInWithEmailAndPassword(
-            auth,
-            email,
-            password
-          );
-          console.log("User Logged in");
-          // determine if user is admin or donor
-          //   const userID = auth.currentUser.uid;
-          const docRef = doc(db, "Users", auth.currentUser.uid);
-          const docsnap = await getDoc(docRef);
-          if (docsnap.exists()) {
-            if (docsnap.data().accountType === "organization") {
-              console.log(docsnap.data().accountType);
-              window.location.assign("../../faq.html");
+// LOGIN
+$(".reusable-component").on("click", ".login-btn, .login-btn-large", async function loginWithEmailPassword() {
+    const emaillogin = document.getElementById("login_page_email_input").value;
+    const passwordlogin = document.getElementById("login_page_password_input").value;
+    console.log(emaillogin, passwordlogin);
+    try {
+      const email = emaillogin;
+      const password = passwordlogin;
+      onAuthStateChanged(auth, async (user) => {
+        try {
+          if (!user) {
+            const userCredentials = await signInWithEmailAndPassword(
+              auth,
+              email,
+              password
+            );
+            console.log("User Logged in");
+            // determine if user is admin or donor
+            //   const userID = auth.currentUser.uid;
+            const docRef = doc(db, "Users", auth.currentUser.uid);
+            const docsnap = await getDoc(docRef);
+            if (docsnap.exists()) {
+              if (docsnap.data().accountType === "organization") {
+                console.log(docsnap.data().accountType);
+                window.location.assign("../../faq.html");
+              }
             }
+            location.reload(true);
           }
-          location.reload(true);
+        } catch (err) {
+          alert(err.message);
         }
-      } catch (err) {
-        alert(err.message);
-      }
-    });
-
-    // console.log(userCredentials.user);
-  } catch (err) {
-    alert(err.message);
-  }
-}
-
-const donorName = document.getElementById("donor-full-name");
-const donorEmail = document.getElementById("donor-email");
-const donorNumber = document.getElementById("donor-number");
-const donorPassword = document.getElementById("donor-password");
-const donorConfirmPassword = document.getElementById("donor-confirm-password");
-// Sign Up as Donor
-async function createDonorWithEmaillPass() {
-  try {
-    console.log("creatingUser");
-    const email = donorEmail.value;
-    const password = donorPassword.value;
-
-    if (password !== donorConfirmPassword.value) {
-      alert("passwords do not match!");
-      return;
+      });
+  
+      // console.log(userCredentials.user);
+    } catch (err) {
+      alert(err.message);
     }
-    const userCredentials = await createUserWithEmailAndPassword(
-      auth,
-      email,
-      password
-    );
-    console.log("User Created");
-    const docRef = await setDoc(doc(db, "Users", auth.currentUser.uid), {
-      id: auth.currentUser.uid,
-      name: donorName.value,
-      email: donorEmail.value,
-      number: donorNumber.value,
-      accountType: "donor",
-    });
-    console.log("User document created");
+});
 
-    const bookmarks = addDoc(
-      collection(db, "Users", auth.currentUser.uid, "Bookmarks"),
-      {}
-    );
+// PARTNER SIGNUP
+$(".reusable-component").on("click", ".partner-signup-btn, .partner-signup-btn-large", async function createOrgWithEmaillPass() {
+    const orgName = document.getElementById("org-name").value;
+    const orgEmail = document.getElementById("org-email").value;
+    const orgNum = document.getElementById("org-number").value;
+    console.log(orgName, orgEmail, orgNum);
+    try {
+        throw new Error("Cannot create admin account at this time.")
+    } catch (err) {
+        alert(err.message);
+    }
+    // $(function (){
+    //     $("#modal-partner").modal("toggle");
+    // })
+});
 
-    console.log("bookmarks created");
+// DONOR SIGN UP
+$(".reusable-component").on("click", ".donor-signup-btn, .donor-signup-btn-large", async function createDonorWithEmaillPass() {
+    const donorName = document.getElementById("donor-full-name").value;
+    const donorEmail = document.getElementById("donor-email").value;
+    const donorNumber = document.getElementById("donor-number").value;
+    const donorPassword = document.getElementById("donor-password").value;
+    const donorConfirmPassword = document.getElementById("donor-confirm-password").value;
+    console.log(donorName, donorEmail, donorNumber, donorPassword, donorConfirmPassword);
+    try {
+      console.log("creatingUser");
+      const email = donorEmail;
+      const password = donorPassword;
+  
+      if (password !== donorConfirmPassword) {
+        alert("Passwords do not match!");
+        return;
+      }
+      const userCredentials = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log("User Created");
+      const docRef = await setDoc(doc(db, "Users", auth.currentUser.uid), {
+        id: auth.currentUser.uid,
+        name: donorName,
+        email: donorEmail,
+        number: donorNumber,
+        accountType: "donor",
+      });
+      console.log("User document created");
+  
+      const bookmarks = addDoc(
+        collection(db, "Users", auth.currentUser.uid, "Bookmarks"),
+        {}
+      );
+  
+      console.log("bookmarks created");
+  
+      const donations = addDoc(
+        collection(db, "Users", docRef.id, "Donations"),
+        {}
+      );
+      console.log("donations collection created");
+      alert("You have created an account!");
+      location.reload(true);
+    } catch (err) {
+      alert(err.message);
+    }
+    // $(function () {
+    //   $("#modal-donor").modal("toggle");
+    // });
+});
 
-    const donations = addDoc(
-      collection(db, "Users", docRef.id, "Donations"),
-      {}
-    );
-    console.log("donations collection created");
-    alert("You have created an account!");
-    location.reload(true);
-  } catch (err) {
-    alert(err.message);
-  }
-  $(function () {
-    $("#modal-donor").modal("toggle");
-  });
-}
+// FORGOT PASSWORD
+$(".reusable-component").on("click", ".reset-btn, .reset-btn-large", function resetEmail() {
+    const emailInput = document.getElementById("forgot-password-email").value;
+    const email = emailInput;
+    console.log(email);
+    if (email === null) {
+      throw new Error("Email Field is Empty");
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert("Password reset email sent!");
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+});
 
-const orgName = document.getElementById("org-name");
-const orgEmail = document.getElementById("org-email");
-const orgNum = document.getElementById("org-number");
-async function createOrgWithEmaillPass() {
-  try {
-  } catch (err) {
-    alert(err.message);
-  }
+// const forgotPasswordLink = document.getElementById("send-reset-email-btn");
+// forgotPasswordLink.addEventListener("click", resetEmail)
 
-  $(function () {
-    $("#modal-partner").modal("toggle");
-  });
-}
-
-function resetEmail() {
-  const emailInput = document.getElementById("forgot-password-email");
-  const email = emailInput.value;
-  if (email === null) {
-    throw new Error("Email Field is Empty");
-  }
-  sendPasswordResetEmail(auth, email)
-    .then(() => {
-      alert("Password reset email sent!");
-    })
-    .catch((error) => {
-      alert(error.message);
-    });
-}
-
-// const forgotPasswordLink = document.getElementById('send-reset-email-btn');
-// forgotPasswordLink.addEventListener('click', resetEmail)
 
 // const loginBTN = document.getElementById()
 var loginBtn = document.getElementById("login-btn");
@@ -194,24 +180,7 @@ async function donate() {
   const bookmarks = addDoc(collection(db, "Users", docRef.id, "Bookmarks"), {});
 }
 
-// async function signOutUser(){
-//     try{
-//         const signedOut =await signOut(auth)
-//         console.log('logged out');
-//         location.reload(true);
-//     } catch(error){
-//         console.log(error.message);
-//         console.log('in signoutuser')
-//     }
-// }
-
-// $(document).read(function(){
-//         $('#signout-btn').on('click', function(){
-//             console.log(this.id , this.innerHTML);        }
-//         );
-//     }
-// )
-
+// SIGN OUT USER
 $(".sticky-top").on(
   "click",
   ".signout-btn",
@@ -228,18 +197,8 @@ $(".sticky-top").on(
   }
 );
 
-$("#modal-login").on("shown.bs.modal", function () {
-  $(document).on("click", "login-btn", function () {
-    alert("loggedin");
-  });
-});
-
-//   const btn = document.getElementById("login-btn");
-//   btn.addEventListener('click', function(){
-//       alert('potangina')
-//   })
-
-// })
-// signOutUser()
-
-// createUser();
+// $("#modal-login").on("shown.bs.modal", function () {
+//   $(document).on("click", "login-btn", function () {
+//     alert("loggedin");
+//   });
+// });
